d0556067db5d508c215b028fd51bb7721b8e802f
awangui <86366610+awangui@users.noreply.github.com>
2023-01-09 00:46:08 -0800

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     index = 0
-    while index < len(my_list):
-        str = "{}".format(my_list[index])
-        print(str)
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
